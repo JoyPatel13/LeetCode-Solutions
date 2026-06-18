5bd55d81f312b73838e05fa423261baf4515fef9
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-18 11:10:44 +0530

    Update 1334-AngleBetweenHandsOfClockl.cpp

--- 1334-AngleBetweenHandsOfClockl.cpp
+++ 1334-AngleBetweenHandsOfClockl.cpp
@@ -7,12 +7,12 @@ SC = O(1);
 class Solution {
 public:
     double angleClock(int hour, int minutes) {
-        float hr = (float(minutes)/2) + (30*(float(hour)));
-        float min = minutes*6;
-        float ans = hr - min;
+        float DegreeOfHr = (float(minutes)/2) + (30*(float(hour)));
+        float DegreeOfMin = minutes*6;
+        float ans = DegreeOfHr - DegreeOfMin;
         if(ans < 0) ans = -1*ans;
         if(ans>180) return 360-ans;
-        else return ans;
-        return min;
+        return ans;
+
     }
 };
