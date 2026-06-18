585f69b2c20e95c5847a38ca3b447757badcc7c1
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-18 11:06:37 +0530

    Create 1334-AngleBetweenHandsOfClockl.cpp

---
+++ 1334-AngleBetweenHandsOfClockl.cpp
@@ -0,0 +1,26 @@
+/*
+TC = O(1)
+SC = O(1);
+
+*/
+
+
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        float hr = (float(minutes)/2) + (30*(float(hour)));
+        float min = minutes*6;
+        float ans = hr - min;
+        if(ans < 0){
+            ans = -1*ans;
+        }
+        if(ans>180){
+            return 360-ans;
+
+        }
+        else{
+            return ans;
+        }
+        return min;
+    }
+};
