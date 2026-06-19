6ac5435c82f991e11d6a97adbe0f2c2f4d4d268c
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-06-19 11:59:16 +0530

    Create 1732-FIndTheHighestAltitude.cpp

---
+++ 1732-FIndTheHighestAltitude.cpp
@@ -0,0 +1,18 @@
+/*
+TC = O(n)
+SC = O(1)
+
+*/
+
+class Solution {
+public:
+    int largestAltitude(vector<int>& gain) {
+        int currentAltitude = 0 ;
+        int highestAltitude = currentAltitude;
+        for(int altitudeGain : gain){
+            currentAltitude+= altitudeGain;
+            highestAltitude =  max(highestAltitude , currentAltitude);
+        }
+        return highestAltitude;
+    }
+};
