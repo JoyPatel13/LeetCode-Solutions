d02220e55aa7438e81bd126ae266a1c3d412d449
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-07-01 23:15:27 +0530

    Create 1021-remove-outermost-parentheses.cpp

---
+++ 1021-remove-outermost-parentheses.cpp
@@ -0,0 +1,27 @@
+/*
+TC = O(n)
+
+*/
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string ans ;
+        int balance = 0 ;
+        for(int i=0 ;i<s.size();i++){
+            if(s[i]=='('){
+                if(balance>0) {
+                    ans+= s[i];
+                }
+                balance++;
+            }
+            else{
+                balance -- ;
+                if(balance>0){
+
+                    ans+= s[i];
+                }
+            }
+        }
+        return ans;
+    }
+};
