64905717a6a9b2fc4688c5befb9cea5036712155
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-07-02 10:22:53 +0530

    Create 1846-MaximumElementAfterDecreasingAndRearranging.cpp

---
+++ 1846-MaximumElementAfterDecreasingAndRearranging.cpp
@@ -0,0 +1,18 @@
+/*
+TC = O(n)
+SC + O(1)
+*/
+
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin() ,arr.end());
+        arr[0] = 1 ;
+        for(int i =1 ; i<arr.size() ; i++){
+            if(abs(arr[i] - arr[i-1]>1)){
+                arr[i] = arr[i-1]+ 1;
+            }
+        }
+        return arr.back();
+    }
+};
