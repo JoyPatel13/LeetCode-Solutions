f8efec611d5dba2c5bdbd969421591463ae68d61
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-07-07 11:48:25 +0530

    Create 3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp

---
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -0,0 +1,19 @@
+/*
+TC = O(n)
+*/
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        long long x = 0;
+        long long sum = 0;
+        string s = to_string(n);
+        for(char c : s){
+            int digit = c - '0';
+            sum+= d;
+            if(d>0){
+                x = x * 10 + d;
+            }
+        }
+        return x*sum;
+    }
+};
