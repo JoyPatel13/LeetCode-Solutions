a1db25d6fcbe69dd35ff28ab89b292e5ebd16ecc
Patel Joy Rajeshkumar <226872928+JoyPatel13@users.noreply.github.com>
2026-07-10 16:38:57 +0530

    Update 222-CountCompleteTreeNodes.cpp

--- 222-CountCompleteTreeNodes.cpp
+++ 222-CountCompleteTreeNodes.cpp
@@ -23,3 +23,32 @@ public:
         return ans;
     }
 };
+
+
+//Optimal solution
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root == nullptr) return 0 ;
+        int lh = findLeftHeight(root);
+        int rh = findRightHeight(root);
+        if(lh == rh) return (1<<lh) - 1;
+        return 1 + countNodes(root->left ) + countNodes(root->right);
+    }
+    int findLeftHeight(TreeNode* node){
+        int h= 0 ;
+        while(node){
+            h++;
+            node =node->left;
+        }
+        return h;
+    }
+    int findRightHeight(TreeNode* node){
+        int h = 0;
+        while(node){
+            h++;
+            node=node->right;
+        }
+        return h;
+    }
+};
