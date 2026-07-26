4f3d88cfca9a445961deab8a527f0d4702939953
Joy Patel <226872928+JoyPatel13@users.noreply.github.com>
2026-07-26 10:38:53 +0530

    Time: 4 ms (60.1%), Space: 31.4 MB (76.45%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.cpp
@@ -4,10 +4,9 @@ public:
         int n = nums.size();
         sort(nums.begin() , nums.end()); 
         int max1 = nums[n-1]*nums[n-2]*nums[n-3];
-        int low = 0 , high = n-1 ;
         int max2 = nums[0]*nums[1];
-        if(max2>0)max2*=nums[n-1];
-        return max(max1,max2);
+        if(max2>0) max2*=nums[n-1];
+        return max1>max2 ? max1 : max2;
 
     }
 };
