63f050e3de80956c01198807cd0cdf4ecd09c2b7
Joy Patel <226872928+JoyPatel13@users.noreply.github.com>
2026-08-02 15:52:35 +0530

    Time: 0 ms (100%), Space: 17.9 MB (99.38%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,25 +1,28 @@
 class Solution {
-private:
-    void dfs(int row ,int col , vector<vector<int>>& ans , vector<vector<int>>& image , int newColor , int delRow[] , int delCol[] , int iniColor){
-        ans[row][col] = newColor;
-        int n = image.size();
-        int m = image[0].size();
-        //check for neighbour
-        for(int i =0 ;i <4 ;i++){
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor){
-                dfs(nrow , ncol ,ans , image , newColor , delRow ,delCol , iniColor);
-            }
-        }
-    }
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int iniColor = image[sr][sc];
-        vector<vector<int>> ans = image ; 
-        int delRow[] = {-1 , 0 , 1 , 0};
-        int delCol[] = {0 ,1,0,-1};
-        dfs(sr ,sc,ans,image , color , delRow , delCol , iniColor);
-        return ans;
+        if(color!=iniColor){
+            dfs(image , sr ,sc ,iniColor , color);
+        }
+        return image;
+    }
+private:
+    void dfs(vector<vector<int>>& image, int r, int c, int iniColor, int color){
+        if(image[r][c] == iniColor){
+            image[r][c] = color;
+            if(r>=1 ){
+                dfs(image , r-1 ,c , iniColor , color );
+            }
+            if(c>=1){
+                dfs(image, r, c-1 , iniColor ,color);
+            }
+            if(r+1 < image.size()){
+                dfs(image , r+1 , c ,iniColor ,color);
+            }
+            if(c+1 < image[0].size()){
+                dfs(image ,r ,c+1 , iniColor , color);
+            }
+        } 
     }
 };
