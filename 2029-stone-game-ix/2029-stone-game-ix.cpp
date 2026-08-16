2de8e1c42438970f933d9b508263b6a638810a35
Joy Patel <226872928+JoyPatel13@users.noreply.github.com>
2026-08-16 12:14:52 +0530

    Time: 5 ms (48.73%), Space: 131.2 MB (69.29%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt0 =0 , cnt1 =0 , cnt2 =0 ;
+        for(int num : stones){
+            int val = num%3 ;
+            if(val%3 == 0) cnt0++;
+            else if(val%3 ==1 )cnt1++;
+            else cnt2++;
+        }
+
+        if(cnt0 %2 ==0 ){
+            return cnt1>=1 && cnt2>=1;
+
+        }
+        return abs(cnt1 - cnt2) > 2;
+    }
+};
