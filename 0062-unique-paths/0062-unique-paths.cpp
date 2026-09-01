0e2279214e65d7ed055d4ad0f2a3650b45f8090f
Joy Patel <226872928+JoyPatel13@users.noreply.github.com>
2026-09-01 21:46:31 +0530

    Time: 0 ms (100%), Space: 9.5 MB (31.02%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -5,15 +5,28 @@ public :
         if(i < 0 || j < 0) return 0 ;
 
         if(dp[i][j] != -1 )return dp[i][j];
-        int left =  f(i , j-1 , dp);
         int up  = f(i-1 , j , dp);
+        int left =  f(i , j-1 , dp);
         int ans = left + up;
         return dp[i][j] = ans;
     }
 public:
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m ,vector<int>(n,-1));
-
-        return f(m-1 ,n-1, dp);
+        dp[0][0] = 1;
+        int ans =0 ;
+        for(int i =0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 && j==0 ) continue;
+                else{
+                    int up =0 ;
+                    int left= 0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
