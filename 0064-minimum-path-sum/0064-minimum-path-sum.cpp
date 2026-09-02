77cc67dbb31765d6e94feaca1101d52b16a138f5
Joy Patel <226872928+JoyPatel13@users.noreply.github.com>
2026-09-02 21:49:30 +0530

    Time: 0 ms (100%), Space: 16.4 MB (42.21%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -15,7 +15,23 @@ public:
         int n = grid.size() ; 
         int m = grid[0].size();
         vector<vector<int>>dp(n , vector<int>(m ,-1));
-        return f(n-1 ,m-1 , grid , dp);
+
+        dp[0][0] = grid[0][0] ;
+        for(int i =0 ;i< n;i++){
+            for(int j =0 ;j< m;j++){
+                int up = 1e9 ;
+                int left = 1e9 ;
+                if(i == 0 && j==0 )continue;
+                if(i > 0) {
+                    up = grid[i][j] + dp[i-1][j];
+                }
+                if(j>0 ){
+                    left = grid[i][j] + dp[i][j-1];
+                }
+                dp[i][j] = min(left, up);
+            }
+        }
+        return dp[n-1][m-1];
 
     }
 };
