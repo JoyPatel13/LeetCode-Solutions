4d7f0e2d60ab57fa5a5c9c7b4798aa3c02179b40
Joy Patel <226872928+JoyPatel13@users.noreply.github.com>
2026-09-10 20:48:27 +0530

    Time: 41 ms (32.9%), Space: 36.3 MB (66.59%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -13,13 +13,34 @@ public :
         }
 
         notTake = f(index-1, amount, coins, dp);
-        
+
         return dp[index][amount] =  take + notTake; 
     }
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size() ;
-        vector<vector<int>> dp(n, vector<int>(amount+ 1, -1));
-        return f(n-1, amount , coins, dp);
+        vector<vector<int>> dp(n, vector<int>(amount+ 1, 0));
+        for(int i =0 ;i<n;i++){
+            dp[i][0] = 1;
+        }
+        for(int index = 0 ;index<n;index++){
+            for(int amt =1 ;amt <=amount;amt++){
+                long long take= 0;
+                long long notTake =0 ;
+
+                
+
+                if(coins[index]<= amt){
+                    take = dp[index] [amt - coins[index]];
+                }
+                if(index>0){
+
+                    notTake = dp[index-1][amt]; 
+                }
+
+                 dp[index][amt] =  take + notTake;
+            }
+        }
+        return (int)dp[n-1][amount];
     }
 };
